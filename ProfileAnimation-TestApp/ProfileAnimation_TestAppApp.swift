9a39b7d6651ef5d25723875605361e7a1f543dec
Ridge Williams <ridgewilliams1069@gmail.com>
2022-12-05 07:49:55 -0800

    Initial Commit

---
+++ ProfileAnimation-TestApp/ProfileAnimation_TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileAnimation_TestAppApp.swift
+//  ProfileAnimation-TestApp
+//
+//  Created by Ridge Williams on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ProfileAnimation_TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
